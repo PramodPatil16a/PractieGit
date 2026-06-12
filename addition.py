3d89fb9add5055d4937bbbf06435e992a1b7d0fc
Pramod <pramod.patil16a@gmail.com>
2026-06-12 14:42:07 +0530

    second commit

---
+++ addition.py
@@ -0,0 +1,13 @@
+# simple addition
+a = int(input("Enter a number: "))
+b = int(input("Enter b number: "))
+print("Sum is:", a + b)
+
+#using funtion
+def add_numbers():
+    c = int(input("Enter c number: "))
+    d = int(input("Enter d number: "))
+    print("Sum is:", c + d)
+
+# Call the function
+add_numbers()
