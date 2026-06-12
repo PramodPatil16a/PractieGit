f52ecc32e9a84d1e6a523dc159fc48c08893618c
Pramod <pramod.patil16a@gmail.com>
2026-06-12 15:05:58 +0530

    Thirt commit

--- sub.py
+++ sub.py
@@ -1,6 +1,29 @@
+
+# simple substraction
 a = int(input("Enter 1st number: "))
 b = int(input("Enter 2nd number: "))
 c = int(input("Enter 3rd number: "))
 print("substaction of numbers :" , (a-b))
 print("substaction of numbers :" , (a-c))
+
+# using input interactive
+def numbers(a, b, c):
+    return a - b - c
+
+# Take values from user
+a = int(input("Enter 1st number: "))
+b = int(input("Enter 2nd number: "))
+c = int(input("Enter 3rd number: "))
+
+# Call function with those values
+result = numbers(a, b, c)
+print("Result:", result)
+
+#passing values
+def numbers(a, b, c):
+    return a - b - c
+
+# Directly pass values
+result = numbers(10, 3, 2)
+print("Result:", result)
 
