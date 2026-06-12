ff0b3587e6530e0f4633e66cf52b61c8e0c7a5b8
Pramod <pramod.patil16a@gmail.com>
2026-06-12 14:56:16 +0530

    Third commit

---
+++ sub.py
@@ -0,0 +1,6 @@
+a = int(input("Enter 1st number: "))
+b = int(input("Enter 2nd number: "))
+c = int(input("Enter 3rd number: "))
+print("substaction of numbers :" , (a-b))
+print("substaction of numbers :" , (a-c))
+
